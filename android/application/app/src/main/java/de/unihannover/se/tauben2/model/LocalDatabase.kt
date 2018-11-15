7239d10363c7c1b9ef759ef6d69ea6f980720b86
Fabian <Fab.beringer@gmail.com>
2018-11-16 00:34:42 +0100

    implement basic room database file

--- android/application/app/src/main/java/de/unihannover/se/tauben2/model/LocalDatabase.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/LocalDatabase.kt
@@ -1,3 +1,25 @@
 package de.unihannover.se.tauben2.model
 
-class LocalDatabase{}
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class LocalDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        var DATABASE: LocalDatabase? = null
+
+        fun getDatabase(context: Context) = DATABASE ?: synchronized(this) {
+            DATABASE ?: buildDatabase(context).also { DATABASE = it }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+                context.applicationContext,
+                LocalDatabase::class.java,
+                "local-database"
+        ).build()
+    }
+}
