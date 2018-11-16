63d151a028bf694ec237942df0698f6abe373eed
Alexander Poets <alexander.poets@gmx.com>
2018-11-16 23:19:59 +0100

    Added routes folder and Procfile. Now ready to be deployed to Heroku.

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 from instance.config import app_config
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 def create_app(config_name):
-    from api.models.test import Test
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile("config.py")
-    with app.app_context():
-        db.init_app(app)
-        result = Test.get_all()
-        print(result)
+    db.init_app(app)
+    ma.init_app(app)
+
+    from api.routes import test
+    app.register_blueprint(test.bp)
+
     return app
