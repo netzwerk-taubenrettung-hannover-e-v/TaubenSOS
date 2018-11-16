4b7dd7eb2de68ef31855d66fc55bead00ed0c3a0
ljohn <john.lena@t-online.de>
2018-11-16 12:19:08 +0100

    new class enum converter

---
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/EnumConverter.kt
@@ -0,0 +1,15 @@
+package de.unihannover.se.tauben2.model
+
+import java.security.Permissions
+import kotlin.reflect.KClass
+
+class EnumConverter{
+
+    companion object {
+      //  fun toInteger(enum:Permission){
+        // }
+    }
+
+
+
+}
