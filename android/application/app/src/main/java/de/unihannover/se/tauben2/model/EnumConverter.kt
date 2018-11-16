c406716f0fb3e502e688227a68e4cd95a97fd76d
ljohn <john.lena@t-online.de>
2018-11-16 12:41:31 +0100

    new gradle files

--- android/application/app/src/main/java/de/unihannover/se/tauben2/model/EnumConverter.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/EnumConverter.kt
@@ -1,14 +1,16 @@
 package de.unihannover.se.tauben2.model
 
+import android.arch.persistence.room.TypeConverter
 import java.security.Permissions
 import kotlin.reflect.KClass
 
 class EnumConverter{
 
     companion object {
-      //  fun toInteger(enum:Permission){
-        // }
+        @TypeConverter
+        fun toInteger(enum :Permission) = enum.ordinal
     }
+
 
 
 
