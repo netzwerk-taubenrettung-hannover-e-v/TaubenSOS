a8e941770a044d6e1ccd2dc47ac624cd87168e4f
Fabian <Fab.beringer@gmail.com>
2018-11-17 19:38:36 +0100

    add missing attributes to Injury class

--- android/application/app/src/main/java/de/unihannover/se/tauben2/model/entity/Injury.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/entity/Injury.kt
@@ -1,16 +1,19 @@
 package de.unihannover.se.tauben2.model.entity
 
+import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 
 
 /**
  * checklist for what injuries a pigeon has
  */
-
+@Entity(tableName = "injury")
 data class Injury(@PrimaryKey val id: Int,
                   var footOrLeg: Boolean,
                   var wing: Boolean,
+                  var head: Boolean,
                   var openWound: Boolean,
                   var paralyzedOrFlightless: Boolean,
+                  var chick: Boolean,
                   var other: Boolean
 )
