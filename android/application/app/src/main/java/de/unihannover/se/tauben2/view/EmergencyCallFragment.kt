ae91e91b1ae36a8665d7ff70f5612c944fc870fb
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-20 18:42:53 +0100

    Added More menu functionality. Added Fragments for EmergencyCall, Contact.

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/EmergencyCallFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/EmergencyCallFragment.kt
@@ -7,16 +7,16 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import de.unihannover.se.tauben2.R
 
-class CounterFragment : Fragment() {
+class EmergencyCallFragment : Fragment() {
 
     companion object {
-        fun newInstance(): CounterFragment {
-            return CounterFragment()
+        fun newInstance(): EmergencyCallFragment {
+            return EmergencyCallFragment()
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_counter, container, false)
+        return inflater.inflate(R.layout.fragment_emergency_call, container, false)
     }
 }
