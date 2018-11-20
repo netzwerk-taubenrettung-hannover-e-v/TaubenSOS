655e481fb8d3366c11daaa98d18221e7eadf6494
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-20 16:54:40 +0100

    Added Fragments for Cases, Graphs, More.

---
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/GraphsFragment.kt
@@ -0,0 +1,23 @@
+package de.unihannover.se.tauben2.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import de.unihannover.se.tauben2.R
+
+class GraphsFragment : Fragment() {
+
+
+    companion object {
+        fun newInstance(): GraphsFragment {
+            return GraphsFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_graphs, container, false)
+    }
+}
