acf6c3644608a5d2ef664a0bd4190edfac6c27d8
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-20 16:24:18 +0100

    Rebased with Developer Branch

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/MainActivity.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/MainActivity.kt
@@ -2,12 +2,37 @@ package de.unihannover.se.tauben2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import de.unihannover.se.tauben2.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {item ->
+            when (item.itemId) {
+                R.id.item_news -> {
+                    val fragment = NewsFragment.newInstance()
+                    replaceFragment(fragment)
+                    // true
+                }
+                R.id.item_counter -> {
+                    val fragment = CounterFragment.newInstance()
+                    replaceFragment(fragment)
+                    // true
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_fragment, fragment)
+        fragmentTransaction.commit()
     }
 }
+
