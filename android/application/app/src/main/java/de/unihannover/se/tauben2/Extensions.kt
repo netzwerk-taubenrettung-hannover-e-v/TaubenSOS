f77e07b63b86be1614e3e8cd8d637dea4b197c18
Andreas Domin <andreasdomin97@gmail.com>
2018-11-21 14:10:54 +0100

    Added ViewModelFactory to get context in ViewModels

--- android/application/app/src/main/java/de/unihannover/se/tauben2/Extensions.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/Extensions.kt
@@ -1,6 +1,12 @@
 package de.unihannover.se.tauben2
 
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 import de.unihannover.se.tauben2.model.network.Resource
+import de.unihannover.se.tauben2.viewmodel.BaseViewModel
+import de.unihannover.se.tauben2.viewmodel.ViewModelFactory
 import retrofit2.Response
 
 fun <ResultType> Response<ResultType>.toResource(): Resource<ResultType> {
@@ -15,4 +21,17 @@ fun <ResultType> Response<ResultType>.toResource(): Resource<ResultType> {
         }
         else -> Resource.error(error)
     }
+}
+
+fun <T: BaseViewModel> FragmentActivity.getViewModel(modelClass: Class<T>): T  = ViewModelProviders.of(this, ViewModelFactory(this)).get(modelClass)
+
+fun <X> LiveDataRes<List<X>>.filter(func: (X) -> Boolean): LiveDataRes<List<X>> = Transformations.map(this) {
+    var result: Resource<List<X>>? = null
+    if(it != null){
+        if(it.status.isSuccessful())
+            result = Resource.success(it.data?.filter(func))
+        if(it.status.isLoading())
+            result = Resource.loading()
+    } else result = Resource.error(it?.message)
+    result
 }
