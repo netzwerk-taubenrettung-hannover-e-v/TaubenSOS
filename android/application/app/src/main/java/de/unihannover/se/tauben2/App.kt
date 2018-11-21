b27b5e6dfbfb632a785c6b2f7ac4be95afb42fa6
Andreas Domin <andreasdomin97@gmail.com>
2018-11-21 11:08:10 +0100

    Added CallAdapterFactory to Retrofit

--- android/application/app/src/main/java/de/unihannover/se/tauben2/App.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/App.kt
@@ -1,6 +1,7 @@
 package de.unihannover.se.tauben2
 
 import android.app.Application
+import de.unihannover.se.tauben2.model.network.LiveDataCallAdapterFactory
 import de.unihannover.se.tauben2.model.network.NetworkService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -19,7 +20,9 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(LiveDataCallAdapterFactory()).build()
 
         mNetworkService = retrofit.create(NetworkService::class.java)
     }
