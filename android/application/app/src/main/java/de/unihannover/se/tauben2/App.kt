20aa5e200a7207a95ae3a05fd5c01b190ce1aae7
Andreas Domin <andreasdomin97@gmail.com>
2018-11-21 11:06:23 +0100

    Added application class

---
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/App.kt
@@ -0,0 +1,26 @@
+package de.unihannover.se.tauben2
+
+import android.app.Application
+import de.unihannover.se.tauben2.model.network.NetworkService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://tauben2.herokuapp.com/api/"
+
+class App: Application() {
+
+
+    companion object {
+        private lateinit var mNetworkService: NetworkService
+
+        fun getNetworkService() = mNetworkService
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+
+        mNetworkService = retrofit.create(NetworkService::class.java)
+    }
+}
