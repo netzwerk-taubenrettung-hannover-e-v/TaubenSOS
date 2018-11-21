42f3545f66e32d2472a4e704acff21d26a73869d
Andreas Domin <andreasdomin97@gmail.com>
2018-11-21 12:53:06 +0100

    Added CaseViewModel; Added Cases to repository

---
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/viewmodel/CaseViewModel.kt
@@ -0,0 +1,10 @@
+package de.unihannover.se.tauben2.viewmodel
+
+import android.content.Context
+import de.unihannover.se.tauben2.LiveDataRes
+import de.unihannover.se.tauben2.model.entity.Case
+
+class CaseViewModel(context: Context): BaseViewModel(context) {
+
+    val cases: LiveDataRes<List<Case>> = repository.getCases()
+}
