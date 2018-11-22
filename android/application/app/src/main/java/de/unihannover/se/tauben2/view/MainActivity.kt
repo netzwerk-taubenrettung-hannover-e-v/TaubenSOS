caeaa25066f5ea4033ba6033153cdcac23f722a2
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-22 22:14:29 +0100

    Added Recyclerlist functionality for CounterFragment

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/MainActivity.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/MainActivity.kt
@@ -8,13 +8,11 @@ import android.view.View
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.appcompat.widget.ToolbarWidgetWrapper
 import androidx.fragment.app.Fragment
 import de.unihannover.se.tauben2.R
 import de.unihannover.se.tauben2.filter
 import de.unihannover.se.tauben2.getViewModel
 import de.unihannover.se.tauben2.viewmodel.CaseViewModel
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), FragmentChangeListener {
@@ -24,10 +22,9 @@ class MainActivity : AppCompatActivity(), FragmentChangeListener {
         setContentView(R.layout.activity_main)
 
         // Toolbar Settings
-        var toolbar = findViewById(R.id.toolbar) as Toolbar
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        // toolbar.setLogo(R.drawable.ic_logo)
 
         // Bottom Navigation Event Handler
         bottom_navigation.setOnNavigationItemSelectedListener {item ->
