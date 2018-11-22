a862d50f2c3b314c623e63793c3cb236d05a9d7d
Fabian <fab.beringer@gmail.com>
2018-11-22 17:20:26 +0100

    add StringList converter for storing String lists directly in the Database

---
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/ListStringConverter.kt
@@ -0,0 +1,27 @@
+package de.unihannover.se.tauben2.model
+
+import androidx.room.TypeConverter
+
+
+class ListConverter<String> {
+    companion object {
+        /**
+         * Converts a list of Strings into a String object so it can be stored in the database
+         * @param list List of Strings to be converted
+         */
+        @TypeConverter
+        fun fromStringList(list: List<String>): String {
+            return list.toString().trim('[', ']')
+        }
+
+        /**
+         * Converts a String object representing a list of strings from the database to an actual
+         * list of strings
+         * @param str String to be converted into List
+         */
+        @TypeConverter
+        fun fromString(str: String): List<String> {
+            return str.split(", ")
+        }
+    }
+}
