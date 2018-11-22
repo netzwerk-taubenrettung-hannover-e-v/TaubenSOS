aedcdb770267f3c1aee5133c12cc43b8bf10cd82
Fabian <fab.beringer@gmail.com>
2018-11-22 15:07:36 +0100

    add method in PigeonCounterDao for retrieving PigeonCounters at a certain place and time frame

--- android/application/app/src/main/java/de/unihannover/se/tauben2/model/dao/PigeonCounterDao.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/dao/PigeonCounterDao.kt
@@ -1,6 +1,8 @@
 package de.unihannover.se.tauben2.model.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
 import de.unihannover.se.tauben2.model.entity.PigeonCounter
 
 /**
@@ -9,4 +11,17 @@ import de.unihannover.se.tauben2.model.entity.PigeonCounter
 @Dao
 interface PigeonCounterDao : BaseDao<PigeonCounter> {
 
+    /**
+     * @param latitude latitude coordinate where pigeons were counted
+     * @param longitude longitude coordinate where pigeons were counted
+     * @param from timestamp for earliest pigeon counter object to be retrieved
+     * @param to timestamp for latest pigeon counter object to be retrieved
+     */
+    @Query("""
+        SELECT *
+        FROM population
+        WHERE latitude = :latitude AND longitude = :longitude
+        AND timestamp BETWEEN :from AND :to
+        """)
+    fun getPigeonCounters(latitude: Double, longitude: Double, from: Long, to: Long): LiveData<List<PigeonCounter>>
 }
