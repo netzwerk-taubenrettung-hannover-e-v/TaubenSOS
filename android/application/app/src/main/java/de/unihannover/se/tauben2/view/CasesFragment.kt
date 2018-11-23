fb783f9dea7e661384f17fde68431f9b38dd956f
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-23 17:26:12 +0100

    Added working Mapfragment. Implemented MapFragment to CasesFragment.

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/CasesFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/CasesFragment.kt
@@ -1,35 +1,16 @@
 package de.unihannover.se.tauben2.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import de.unihannover.se.tauben2.R
 import de.unihannover.se.tauben2.model.entity.Case
 import de.unihannover.se.tauben2.model.network.Resource
-import kotlinx.android.synthetic.main.fragment_cases.*
-import kotlinx.android.synthetic.main.fragment_cases.view.*
 
-
-class CasesFragment : Fragment(), Observer<Resource<List<Case>>>, OnMapReadyCallback {
-
-    override fun onMapReady(googleMap: GoogleMap?) {
-        var mMap = googleMap
-
-        // Add a marker in Sydney, Australia, and move the camera.
-        val sydney = LatLng(-34.0, 151.0)
-        mMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-    }
+class CasesFragment : Fragment(), Observer<Resource<List<Case>>> {
 
     override fun onChanged(cases: Resource<List<Case>>?) {
         if (cases?.status?.isSuccessful() == true) {
@@ -45,10 +26,7 @@ class CasesFragment : Fragment(), Observer<Resource<List<Case>>>, OnMapReadyCall
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_cases, container, false)
 
-        var mapFragment = fragmentManager?.findFragmentById(R.id.mapView)
-        Log.d("FRAGEMTNAW", mapFragment.toString())
-        return view
+        return inflater.inflate(R.layout.fragment_cases, container, false)
     }
 }
