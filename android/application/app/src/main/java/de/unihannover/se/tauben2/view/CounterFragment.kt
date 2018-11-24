4531d8ce85490b58218c2333779bdaaf04b2f149
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-24 15:15:34 +0100

    Added onClickListeners to CounterFragment. BugFixes.

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/CounterFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/CounterFragment.kt
@@ -1,14 +1,17 @@
 package de.unihannover.se.tauben2.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import de.unihannover.se.tauben2.R
 import de.unihannover.se.tauben2.R.layout.fragment_counter
+import kotlinx.android.synthetic.main.fragment_counter.*
 
-class CounterFragment : Fragment() {
+class CounterFragment : Fragment(), View.OnClickListener {
 
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<AdapterList.ViewHolder>? = null
@@ -23,7 +26,36 @@ class CounterFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(fragment_counter, container, false)
+
+        view.findViewById<View>(R.id.changedate_button).setOnClickListener(this)
+        view.findViewById<View>(R.id.plus_button).setOnClickListener(this)
+        view.findViewById<View>(R.id.minus_button).setOnClickListener(this)
+        view.findViewById<View>(R.id.send_count_button).setOnClickListener(this)
+
         return view
     }
+
+    override fun onClick(view: View?) {
+
+        when (view) {
+
+            changedate_button -> {
+                // change Date and Time fragment or popup
+            }
+            plus_button -> {
+                val value = if (counter_value.text.isNullOrEmpty()) { 0 } else { Integer.parseInt(counter_value.text.toString()) }
+                if (value < 9999) { counter_value.setText((value + 1).toString()) }
+            }
+            minus_button -> {
+                val value = if (counter_value.text.isNullOrEmpty()) { 0 } else { Integer.parseInt(counter_value.text.toString()) }
+                if (value > 0) { counter_value.setText((value - 1).toString()) }
+            }
+            send_count_button -> {
+                // send data to the server
+            }
+        }
+
+    }
+
 
 }
