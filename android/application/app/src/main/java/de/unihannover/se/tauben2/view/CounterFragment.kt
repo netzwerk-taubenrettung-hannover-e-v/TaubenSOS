bb5e21273a87666289db9958c9ac0fdf31c3ca45
Jan Ellermann <jan.ellermann@t-online.de>
2018-11-24 20:13:47 +0100

    Added current time to CounterFragment.

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/CounterFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/CounterFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import de.unihannover.se.tauben2.R
@@ -13,8 +14,12 @@ import kotlinx.android.synthetic.main.fragment_counter.*
 
 class CounterFragment : Fragment(), View.OnClickListener {
 
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<AdapterList.ViewHolder>? = null
+   private var day : Int = 0
+   private var month : Int = 0
+   private var year : Int = 0
+   private var hour : Int = 0
+   private var minute : Int = 0
+
 
     companion object {
         fun newInstance(): CounterFragment {
@@ -26,6 +31,8 @@ class CounterFragment : Fragment(), View.OnClickListener {
                               savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(fragment_counter, container, false)
+
+        setCurrentTime(view)
 
         view.findViewById<View>(R.id.changedate_button).setOnClickListener(this)
         view.findViewById<View>(R.id.resetdate_button).setOnClickListener(this)
@@ -44,15 +51,27 @@ class CounterFragment : Fragment(), View.OnClickListener {
                 // change Date and Time fragment or popup
             }
             resetdate_button -> {
-                // reset Date and Time to the actual timestamp
+                // reset Date and Time to actual timestamp
             }
             plus_button -> {
-                val value = if (counter_value.text.isNullOrEmpty()) { 0 } else { Integer.parseInt(counter_value.text.toString()) }
-                if (value < 9999) { counter_value.setText((value + 1).toString()) }
+                val value = if (counter_value.text.isNullOrEmpty()) {
+                    0
+                } else {
+                    Integer.parseInt(counter_value.text.toString())
+                }
+                if (value < 9999) {
+                    counter_value.setText((value + 1).toString())
+                }
             }
             minus_button -> {
-                val value = if (counter_value.text.isNullOrEmpty()) { 0 } else { Integer.parseInt(counter_value.text.toString()) }
-                if (value > 0) { counter_value.setText((value - 1).toString()) }
+                val value = if (counter_value.text.isNullOrEmpty()) {
+                    0
+                } else {
+                    Integer.parseInt(counter_value.text.toString())
+                }
+                if (value > 0) {
+                    counter_value.setText((value - 1).toString())
+                }
             }
             send_count_button -> {
                 // send data to the server
@@ -61,5 +80,20 @@ class CounterFragment : Fragment(), View.OnClickListener {
 
     }
 
+    private fun setCurrentTime(view: View?) {
+
+        day = java.util.Calendar.getInstance().get(java.util.Calendar.DAY_OF_MONTH)
+        month = java.util.Calendar.getInstance().get(java.util.Calendar.MONTH)
+        year = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)
+
+        hour = java.util.Calendar.getInstance().get(java.util.Calendar.HOUR_OF_DAY)
+        minute = java.util.Calendar.getInstance().get(java.util.Calendar.MINUTE)
+
+        setTime(view, day, month, year, hour, minute)
+    }
+
+    private fun setTime(view: View?, day : Int, month : Int, year : Int, hour : Int, minute : Int) {
+        view?.findViewById<TextView>(R.id.current_timestamp_value)?.text = String.format("%2d.%2d.%4d %2d:%2d Uhr", day, month, year, hour, minute)
+    }
 
 }
