fd96f532d86d304c580c8feb50e6fc0592e57cd7
hhinrichs <H_Hinrichs@gmx.de>
2018-11-29 11:28:16 +0100

    added register method to user route, added sha256 encryption to column password in user table, added option to activte a user

--- server/app/api/models/user.py
+++ server/app/api/models/user.py
@@ -1,36 +1,38 @@
 from api import db, ma
 
 class User(db.Model):
-    __tablename__="user"
-    username = db.Column(db.String(20), primary_key=True)
-    password = db.Column(db.String(255), nullable=False)
-    phone = db.Column(db.String(30))
-    isAdmin = db.Column(db.Boolean)
+	__tablename__="user"
+	username = db.Column(db.String(20), primary_key=True)
+	password = db.Column(db.String(255), nullable=False)
+	phone = db.Column(db.String(30))
+	isAdmin = db.Column(db.Boolean)
+	isActivated = db.Column(db.Boolean)
 
-    def __init__(self, username, password, phone, isAdmin):
-        self.username = username
-        self.password = password
-        self.phone = phone
-        self.isAdmin = isAdmin
+	def __init__(self, username, password, phone, isAdmin, isActivated):
+		self.username = username
+		self.password = password
+		self.phone = phone
+		self.isAdmin = isAdmin
+		self.isActivated = isActivated
 
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
+	def save(self):
+		db.session.add(self)
+		db.session.commit()
 
-    def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+	def delete(self):
+		db.session.delete(self)
+		db.session.commit()
 
-    def __repr__(self):
-        return "<User: {}>".format(self.username)
+	def __repr__(self):
+		return "<User: {}>".format(self.username)
 
-    @staticmethod
-    def all():
-        return User.query.all()
+	@staticmethod
+	def all():
+		return User.query.all()
 
-    @staticmethod
-    def get(username):
-        return User.query.get(username)
+	@staticmethod
+	def get(username):
+		return User.query.get(username)
 
 class UserSchema(ma.ModelSchema):
     class Meta:
