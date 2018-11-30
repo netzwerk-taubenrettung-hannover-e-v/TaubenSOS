300b9f5687fb582fe777c75abc3a8ea8583b84b0
Sean Westphal <Sean.Westphal97@gmail.com>
2018-11-30 01:18:04 +0100

    Change injuries to be displayed as ListView rather than TextView - currently non-functional

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/recycler/CasesRecyclerFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/recycler/CasesRecyclerFragment.kt
@@ -1,6 +1,7 @@
 package de.unihannover.se.tauben2.view.recycler
 
 import android.location.Location
+import android.os.Bundle
 import android.view.View
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.Observer
@@ -8,8 +9,11 @@ import de.unihannover.se.tauben2.R
 import de.unihannover.se.tauben2.databinding.CardCaseBinding
 import de.unihannover.se.tauben2.getViewModel
 import de.unihannover.se.tauben2.model.entity.Case
+import de.unihannover.se.tauben2.model.entity.Injury
 import de.unihannover.se.tauben2.viewmodel.LocationViewModel
 import kotlinx.android.synthetic.main.card_case.view.*
+import kotlinx.android.synthetic.main.card_case.injury_card_value
+import android.widget.ArrayAdapter
 
 class CasesRecyclerFragment : RecyclerFragment<Case>() {
     override fun getRecylcerItemLayoutId(viewType: Int) = R.layout.card_case
@@ -17,6 +21,16 @@ class CasesRecyclerFragment : RecyclerFragment<Case>() {
     private var mExpandedPosition = -1
 
     private var mLocation: Location? = null
+
+    private var injury = de.unihannover.se.tauben2.model.entity.Injury(1,true,false,true,true,true,false,false) //TEMPORARY FOR TESTING
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val injuryList = convertInjuryToStringList(injury);
+
+        val adapter = ArrayAdapter<String>(this.context, android.R.layout.simple_list_item_1, injuryList)
+        //injury_card_value.adapter=adapter
+    }
 
     override fun onResume() {
         super.onResume()
@@ -50,5 +64,17 @@ class CasesRecyclerFragment : RecyclerFragment<Case>() {
             // TransitionManager.beginDelayedTransition(recyclerView);
             notifyDataSetChanged()
         }
+    }
+
+    private fun convertInjuryToStringList(injury: Injury) : List<String>{
+        val injuryList = mutableListOf<String>()
+        if(injury.footOrLeg) injuryList.add("Verletzter Fuß")
+        if(injury.wing) injuryList.add("Verletzter Flügel")
+        if(injury.head) injuryList.add("Verletzter Kopf")
+        if(injury.openWound) injuryList.add("Offene Wunde")
+        if(injury.paralyzedOrFlightless) injuryList.add("Bewegungs- oder Flugunfähig")
+        if(injury.chick) injuryList.add("Küken")
+        if(injury.other) injuryList.add("Sonstige:")                                       //TODO: Add actual text of other field either here or in CasesRecyclerFragment.kt
+        return injuryList
     }
 }
