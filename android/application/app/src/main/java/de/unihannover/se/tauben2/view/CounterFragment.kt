2d4c17aad7a69b636ef9f68c97cdd62bc7fa55bc
Jan Ellermann <jan.ellermann@t-online.de>
2018-12-01 19:46:20 +0100

    Added 'set position'-button to CounterFragment. Added Crosshair to map.

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/CounterFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/CounterFragment.kt
@@ -2,10 +2,13 @@ package de.unihannover.se.tauben2.view
 
 import android.os.Bundle
 import android.text.InputFilter
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.model.LatLng
+import de.unihannover.se.tauben2.R
 import de.unihannover.se.tauben2.R.layout.fragment_counter
 import de.unihannover.se.tauben2.view.input.InputFilterMinMax
 import kotlinx.android.synthetic.main.fragment_counter.*
@@ -23,8 +26,15 @@ class CounterFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(fragment_counter, container, false)
+        val mapsFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as MapViewFragment
 
         view.counter_value.filters = arrayOf<InputFilter>(InputFilterMinMax(0, 9999))
+
+
+        // OnClickListeners:
+        view.set_position_button.setOnClickListener {
+            mapsFragment.selectPosition()
+        }
 
         view.plus_button.setOnClickListener {
             val value = (counter_value.text.toString().toIntOrNull() ?:0) + 1
@@ -38,6 +48,20 @@ class CounterFragment : Fragment() {
 
         view.resetdate_button.setOnClickListener {
             setCurrentTimestamp()
+        }
+
+        view.send_count_button.setOnClickListener {
+
+            if (mapsFragment.getSelectedPosition() == null) {
+                // Error MSG: No location selected
+            }
+            if (counter_value.text.toString().toInt() == 0) {
+                // Warning MSG: Counter at 0
+            }
+
+            Log.d("COUNTINFO", view?.current_timestamp_value?.text.toString())
+            Log.d("COUNTINFO", mapsFragment.getSelectedPosition()!!.toString())
+            Log.d("COUNTINFO", counter_value.text.toString())
         }
 
         return view
@@ -54,4 +78,3 @@ class CounterFragment : Fragment() {
     }
 
 }
-
