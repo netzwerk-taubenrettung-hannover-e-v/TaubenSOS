8476d450ed61f265527cfc9fa2ab74647749d0cc
Jan Ellermann <jan.ellermann@t-online.de>
2018-12-03 18:00:41 +0100

    Added colors to CaseInfoFragment.

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/CasesInfoFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/CasesInfoFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import de.unihannover.se.tauben2.R
+
 import de.unihannover.se.tauben2.databinding.FragmentCasesinfoBinding
 import de.unihannover.se.tauben2.model.entity.Case
+
+import kotlinx.android.synthetic.main.fragment_casesinfo.view.*
+
 
 class CasesInfoFragment: Fragment() {
 
@@ -22,8 +27,19 @@ class CasesInfoFragment: Fragment() {
         arguments?.getParcelable<Case>("case")?.let {
             binding.c = it
         }
-        return binding.root
 
+        // Set Status Color
+        if (binding.c!!.isClosed) {
+            binding.root.status_card_image_info.setColorFilter(ContextCompat.getColor(context!!, R.color.Green))
+        } else {
+            if (binding.c!!.rescuer != null) {
+                binding.root.status_card_image_info.setColorFilter(ContextCompat.getColor(context!!, R.color.Yellow))
+            } else {
+                binding.root.status_card_image_info.setColorFilter(ContextCompat.getColor(context!!, R.color.Red))
+            }
+        }
+
+        return binding.root
     }
 
 
