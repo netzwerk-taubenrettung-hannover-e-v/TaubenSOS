eab0982bd3165de27ec32d36bd4c303f81f6766d
Fabian <fab.beringer@gmail.com>
2018-12-10 15:03:34 +0100

    create PigeonCounterViewModel and register it in ViewModelFactory

---
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/viewmodel/PigeonCounterViewModel.kt
@@ -0,0 +1,9 @@
+package de.unihannover.se.tauben2.viewmodel
+
+import android.content.Context
+import de.unihannover.se.tauben2.LiveDataRes
+import de.unihannover.se.tauben2.model.entity.PigeonCounter
+
+class PigeonCounterViewModel(context: Context) : BaseViewModel(context) {
+    val pigeonCounters: LiveDataRes<List<PigeonCounter>> = repository.getPigeonCounters()
+}
