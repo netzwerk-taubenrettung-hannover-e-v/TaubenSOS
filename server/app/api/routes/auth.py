1b1239f8e5481276b3219b11376c26b0d1e37a12
pbuschermoehle <pascal.buschermoehle@gmail.com>
2018-12-11 14:56:56 +0100

    Improved error messages

--- server/app/api/routes/auth.py
+++ server/app/api/routes/auth.py
@@ -3,42 +3,75 @@ import jwt
 import uuid
 from datetime import datetime
 from flask import (Blueprint, request)
-from flask import jsonify, abort
+from flask import jsonify
 from api.models.user import (User)
 from api.models.token import (Token)
 
 bp = Blueprint("auth", __name__, url_prefix="/api/auth")
 
+def only(scope):
+	def onlyAdmin(func):
+		def func_wrapper(**kwargs):
+			access_token = request.headers.get("Authorization")
+			if access_token is None:
+				return jsonify(message="Access Token is missing"), 401
+			token_data = decode_access_token(access_token)
+			if token_data is None:
+				return jsonify(message="Invalid token"), 401
+			dbToken = Token.get(token_data['jti'])
+
+			if dbToken is None:
+				return jsonify(message="Invalid access token"), 401
+			values = list(kwargs.values())
+
+			if not kwargs:
+				f = func()
+			else:
+				f = func(values[0])
+
+			if 'member' in scope:
+				return f
+
+			if 'admin' in scope:
+				user = User.get(token_data['username'])
+				if user.isAdmin:
+					return f
+
+			if 'me' in scope:
+				if "username" not in kwargs:
+					return jsonify(message="No permission"), 403
+
+				if kwargs['username'] == token_data['username']:
+					return f
+
+			return jsonify(message="No permission"), 403
+		return func_wrapper
+	return onlyAdmin
+
 @bp.route("/login", methods=["POST"], strict_slashes=False)
 def login():
 	if "username" not in request.json or "password" not in request.json:
-		return abort(400)
+		return jsonify(message="Username or password is missing"), 400
 
 	username = request.json["username"]
 	password = request.json["password"]
 
-	if None in [username, password]:
-		return abort(400)
-
 	user = User.get(username)
 
 	if user is None or password != user.password:
-		return abort(401)
+		return jsonify(message="User does not exist or password is wrong"), 401
 
 	access_token = generate_access_token(user)
 	return jsonify({"token": access_token})
 
 @bp.route('/logout', methods=["DELETE"], strict_slashes=False)
+@only({'member'})
 def logout():
 	access_token = request.headers.get("Authorization")
-	if access_token is None:
-		return abort(401)
 	token_data = decode_access_token(access_token)
 	dbToken = Token.get(token_data['jti'])
-	if dbToken is None:
-		return abort(401)
 	dbToken.delete()
-	return jsonify(success=True), 200
+	return jsonify(message="Logout successful"), 200
 
 def generate_access_token(user):
 	with open(os.path.join(os.path.dirname(__file__), "../keys/private.pem"), 'rb') as f:
@@ -58,42 +91,7 @@ def generate_access_token(user):
 	access_token = jwt.encode(payload, private_key, algorithm='RS256')
 	return access_token.decode()
 
-def only(scope):
-	def onlyAdmin(func):
-		def func_wrapper(**kwargs):
-			access_token = request.headers.get("Authorization")
-			if access_token is None:
-				return abort(401)
-			token_data = decode_access_token(access_token)
-			dbToken = Token.get(token_data['jti'])
 
-			if dbToken is None:
-				return abort(401)
-			values = list(kwargs.values())
-
-			if not kwargs:
-				f = func()
-			else:
-				f = func(values[0])
-
-			if 'member' in scope:
-				return f
-
-			if 'admin' in scope:
-				user = User.get(token_data['username'])
-				if user.isAdmin:
-					return f
-
-			if 'me' in scope:
-				if "username" not in kwargs:
-					return abort(401)
-
-				if kwargs['username'] == token_data['username']:
-					return f
-
-			return abort(401)
-		return func_wrapper
-	return onlyAdmin
 
 def decode_access_token(access_token):
 	with open(os.path.join(os.path.dirname(__file__), "../keys/public.pem"), 'rb') as f:
@@ -102,5 +100,5 @@ def decode_access_token(access_token):
 	try:
 		decoded_token = jwt.decode(access_token.encode(), public_key, algorithm='RS256')
 	except:
-		return abort(401)
+		return None
 	return decoded_token
