cab00d8045e0c108353ad32e421d1017465d85a0
Andreas Domin <andreasdomin97@gmail.com>
2018-12-12 22:30:24 +0100

    Undo accidental deletions in Injury (cherry picked from commit 959d052)

--- android/application/app/src/main/java/de/unihannover/se/tauben2/model/Injury.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/model/Injury.kt
@@ -1,6 +1,9 @@
 package de.unihannover.se.tauben2.model
 
 import android.os.Parcelable
+import de.unihannover.se.tauben2.App
+import de.unihannover.se.tauben2.R
+import de.unihannover.se.tauben2.view.recycler.RecyclerItem
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -11,17 +14,18 @@ data class Injury(var feetTiedTogether: Boolean,
                   var openWound: Boolean,
                   var other: Boolean,
                   var paralyzedOrFlightless: Boolean,
-                  var wing: Boolean
-) : Parcelable {
+                  var wing: Boolean) : Parcelable, RecyclerItem {
+
+    override fun getType(): RecyclerItem.Type = RecyclerItem.Type.ITEM
 
     fun toStringList(): List<String> {
         val injuryList = mutableListOf<String>()
-        if (footOrLeg) injuryList.add("Verletzter Fuß")
-        if (wing) injuryList.add("Verletzter Flügel")
-        if (headOrEye) injuryList.add("Verletzter Kopf")
-        if (openWound) injuryList.add("Offene Wunde")
-        if (paralyzedOrFlightless) injuryList.add("Bewegungs- oder Flugunfähig")
-        if (fledgling) injuryList.add("Küken")
+        if (footOrLeg) injuryList.add(App.context.getString(R.string.injury_foot_leg))
+        if (wing) injuryList.add(App.context.getString(R.string.injury_wings))
+        if (headOrEye) injuryList.add(App.context.getString(R.string.injury_head_eye))
+        if (openWound) injuryList.add(App.context.getString(R.string.injury_open_wound))
+        if (paralyzedOrFlightless) injuryList.add(App.context.getString(R.string.injury_paralyzed_flightless))
+        if (fledgling) injuryList.add(App.context.getString(R.string.injury_fledgling))
         return injuryList
     }
 
