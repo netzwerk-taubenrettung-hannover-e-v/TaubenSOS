709b3304f986ed2c6fb3e254c2bdb9f46fabe6d9
Hauke Hinrichs <H_Hinrichs@gmx.de>
2018-12-14 19:58:36 +0100

    fixed bug with optional json body in population route

--- server/app/api/routes/population.py
+++ server/app/api/routes/population.py
@@ -9,18 +9,26 @@ bp = Blueprint("population", __name__, url_prefix="/api")
 
 @bp.route("/population", methods=["GET"], strict_slashes=False)
 def get_Markers():
+	"""
+	file: ../../docs/population/read_all.yml
+	"""
 	if request.method == "GET":
-		jsonData = request.get_json()
-		data = json.loads(str(jsonData).replace('\'', '\"'))
-		if "lastUpdate" in data:
-			populationMarkers = PopulationMarker.get_newly_updated_markers(convert_timestamp(int(jsonData["lastUpdate"])))
-		else:
+		if request.is_json == True:
+			jsonData = request.get_json()
+			data = json.loads(str(jsonData).replace('\'', '\"'))
+			if "lastUpdate" in data:
+				populationMarkers = PopulationMarker.get_newly_updated_markers(convert_timestamp(int(jsonData["lastUpdate"])))
+			else:
+				return jsonify("Json Body has no key lastUpdate"), 400
 			populationMarkers = PopulationMarker.all()
 		result = [make_json_marker(populationMarker = p) for p in populationMarkers]
 		return jsonify(result)
 
 @bp.route("/population", methods=["POST"], strict_slashes=False)
 def create_marker():
+	"""
+	file: ../../docs/population/create_marker.yml
+	"""
 	if request.method == "POST":
 		json = request.get_json()
 		populationMarker, errors = populationMarker_schema.load(data=json)
@@ -33,6 +41,9 @@ def create_marker():
 
 @bp.route("/population/<populationMarkerID>", methods=["POST"], strict_slashes=False)
 def create_value_for_marker(populationMarkerID):
+	"""
+	file: ../../docs/population/create_value.yml
+	"""
 	if request.method == "POST":
 		populationMarker = PopulationMarker.get(populationMarkerID)
 		if populationMarker is None:
@@ -54,6 +65,9 @@ def create_value_for_marker(populationMarkerID):
 
 @bp.route("/population/<populationMarkerID>", methods=["DELETE"], strict_slashes=False)
 def delete_marker(populationMarkerID):
+	"""
+	file: ../../docs/population/delete_marker.yml
+	"""
 	if request.method == "DELETE":
 		populationMarker = PopulationMarker.get(populationMarkerID)
 		if populationMarker is None:
