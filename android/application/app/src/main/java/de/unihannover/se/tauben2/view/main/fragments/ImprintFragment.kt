6377c81347f87465307351012d7f3f3c7f8ac581
Alexander Poets <alexander.poets@gmx.com>
2019-01-31 11:51:43 +0100

    No more 'Lorem ipsum' inside the imprint and privacy fragments!

--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/main/fragments/ImprintFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/main/fragments/ImprintFragment.kt
@@ -1,5 +1,6 @@
 package de.unihannover.se.tauben2.view.main.fragments
 
+import android.text.Html
 import de.unihannover.se.tauben2.R
 import de.unihannover.se.tauben2.model.TextPage
 
@@ -7,7 +8,7 @@ class ImprintFragment: TextPageFragment(R.string.imprint_title) {
 
     override fun getTextPage(): TextPage {
         return context?.let { cxt ->
-            TextPage(cxt.getString(R.string.imprint_title), cxt.getString(R.string.lorem_ipsum))
+            TextPage(cxt.getString(R.string.imprint_title), cxt.getString(R.string.imprint))
         } ?: TextPage("", "")
     }
 
--- android/application/app/src/main/java/de/unihannover/se/tauben2/view/main/fragments/PrivacyFragment.kt
+++ android/application/app/src/main/java/de/unihannover/se/tauben2/view/main/fragments/PrivacyFragment.kt
@@ -6,7 +6,7 @@ import de.unihannover.se.tauben2.model.TextPage
 class PrivacyFragment: TextPageFragment(R.string.privacy_title) {
     override fun getTextPage(): TextPage {
         return context?.let { cxt ->
-            TextPage(cxt.getString(R.string.privacy_title), cxt.getString(R.string.lorem_ipsum))
+            TextPage(cxt.getString(R.string.privacy_title), cxt.getString(R.string.privacy))
         } ?: TextPage("", "")
     }
 }
